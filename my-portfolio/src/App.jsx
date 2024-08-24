2497e3db6929c98f63b7794d6ebc100af75df5ce
Sudhanshu Singh <111054333+sudhanshusingh07@users.noreply.github.com>
2024-08-24 23:20:22 +0530

    change bg

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -16,6 +16,7 @@ function App() {
         <Route path="/feedback" element={<Feedback />} />
       </Route>
     </Routes>
+    
   )
 }
 
--- my-portfolio/src/Layout.jsx
+++ my-portfolio/src/Layout.jsx
@@ -1,12 +1,12 @@
-import { Outlet } from "react-router-dom"
-import { Navbar } from "./components"
-import Footer from "./components/Footer"
+import { Outlet } from "react-router-dom";
+import { Navbar } from "./components";
+import Footer from "./components/Footer";
 import React from 'react';
-
 
 function Layout() {
     return (
-        <div className="w-full h-full min-h-screen p-5 text-gray-50 dark:bg-gray-primary">
+        <div className="w-full h-full min-h-screen p-5 text-gray-50 dark:bg-gray-primary bg-cover bg-center" 
+             style={{ backgroundImage: "url('/bg.png')" }}>
             <Navbar />
 
             <div className="w-full max-w-2xl mx-auto py-10">
@@ -18,4 +18,4 @@ function Layout() {
     )
 }
 
-export default Layout
+export default Layout;
