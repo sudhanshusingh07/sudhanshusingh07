ac7891653cefcb406258db078f47c5ada42f1edf
Sudhanshu Singh <111054333+sudhanshusingh07@users.noreply.github.com>
2025-03-02 13:40:34 +0530

    Update main.jsx

--- my-portfolio/src/main.jsx
+++ my-portfolio/src/main.jsx
@@ -2,14 +2,7 @@ import React, { lazy, Suspense } from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
-import Loader from './components/ui/Loader.jsx'
 
-// Just to show you my loading screen 😁
-const App = lazy(() =>
-  new Promise(
-    resolve => setTimeout(
-      () => resolve(import('./App.jsx')), 3000
-    )))
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
